9609fa0f75f35b87520432854d4f8e090fc19929
Roberto <robertoholmes@live.com>
2022-05-22 11:44:29 +0100

    Added a README and example projects for sensor reading and TCP networking

---
+++ README.md
@@ -0,0 +1,23 @@
+# AIR
+
+Using an ESP32-S3 to take air quality measurements and send them to a server to be displayed on a local website.
+
+### MCU
+
+Unexpected Maker TinyS3: https://esp32s3.com/tinys3.html
+
+Toolchain: https://docs.espressif.com/projects/esp-idf/en/v4.4.1/esp32s3/index.html
+
+## Commands
+
+### Client
+
+`idf.py set-target esp32-s3`
+
+`idf.py menuconfig`
+
+`idf.py -p PORT flash`
+
+### Server
+
+`go run FILE`
---
+++ dev/Server/tcp/concTCP.go
@@ -0,0 +1,61 @@
+// https://www.linode.com/docs/guides/developing-udp-and-tcp-clients-and-servers-in-go/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var count = 0
+
+func handleConnection(c net.Conn) {
+	fmt.Print(".")
+	for {
+		netData, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		temp := strings.TrimSpace(string(netData))
+		if temp == "STOP" {
+			break
+		}
+		fmt.Println(temp)
+		counter := strconv.Itoa(count) + "\n"
+		c.Write([]byte(string(counter)))
+	}
+	count--
+	c.Close()
+}
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a port number!")
+		return
+	}
+
+	PORT := ":" + arguments[1]
+	l, err := net.Listen("tcp4", PORT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer l.Close()
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go handleConnection(c)
+		count++
+	}
+}
