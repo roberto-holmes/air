24cf8a6f097963b76ef8033ad68375ed00e3e5e5
Roberto <robertoholmes@live.com>
2022-06-24 23:29:48 +0100

    Renamed esp32 sensor module from client to ghost

--- ghost/main/main.c
+++ ghost/main/main.c
@@ -238,7 +238,15 @@ static void scd42_task()
 
 void app_main(void)
 {
-    ESP_ERROR_CHECK(nvs_flash_init());
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    // Initialise TCP/IP stack
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
